c546fb67103b31e9cf3739cff0c8fc7698033777
RonaldoMorales <ronaldo05032002@gmail.com>
2025-01-05 17:50:42 -0300

    feat: updated documentation

--- src/app/jwt/auth.service.ts
+++ src/app/jwt/auth.service.ts
@@ -5,24 +5,61 @@ import { map } from 'rxjs/operators';
 import { CookieService } from 'ngx-cookie-service';
 import { Router } from '@angular/router';
 
+/**
+ * Servicio de autenticación que maneja la autenticación de usuarios en la aplicación.
+ * Provee métodos para iniciar sesión, cerrar sesión, verificar el estado de autenticación,
+ * y gestionar tokens JWT a través de cookies.
+ */
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  /**
+   * URL base del API backend para las operaciones de autenticación.
+   */
   private apiUrl = 'http://localhost:5134/taller-backend/Auth'; 
+
+  /**
+   * Encabezados comunes para las solicitudes HTTP, configurados con el tipo de contenido JSON.
+   */
   private headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+  /**
+   * BehaviorSubject que mantiene el estado de inicio de sesión del usuario.
+   */
   private isLoggedInSubject = new BehaviorSubject<boolean>(false);
+  
+  /**
+   * Observable que emite el estado de inicio de sesión del usuario.
+   */
   isLoggedIn$ = this.isLoggedInSubject.asObservable();
-  private authStatus = new BehaviorSubject<boolean>(false); // BehaviorSubject para almacenar el estado de autenticación
+
+  /**
+   * BehaviorSubject que mantiene el estado de autenticación general.
+   */
+  private authStatus = new BehaviorSubject<boolean>(false);
+  
+  /**
+   * Observable que emite el estado de autenticación general.
+   */
   authStatus$ = this.authStatus.asObservable();
 
-
-
+  /**
+   * Constructor del servicio AuthService.
+   * @param httpClient Instancia de HttpClient para realizar solicitudes HTTP.
+   * @param cookieService Instancia de CookieService para manejar cookies.
+   * @param router Instancia de Router para la navegación dentro de la aplicación.
+   */
   constructor(private httpClient: HttpClient,
-     private cookieService: CookieService,
-      private router: Router) {}
+              private cookieService: CookieService,
+              private router: Router) {}
 
-  // Método para realizar el login y obtener el JWT
+  /**
+   * Realiza el inicio de sesión del usuario y obtiene el JWT.
+   * @param email Correo electrónico del usuario.
+   * @param password Contraseña del usuario.
+   * @returns Observable<{ token: string; nombre: string; rut: string; correo: string }> - Observable que emite los datos del usuario y el token JWT.
+   */
   login(email: string, password: string): Observable<{ token: string; nombre: string; rut: string; correo: string }> {
     const body = { correo: email, contraseña: password };
     return this.httpClient.post<{ token: string; nombre: string; rut: string; correo: string }>(
@@ -39,19 +76,25 @@ export class AuthService {
     );
   }
 
-  
-
-  // Método para guardar el token en una cookie
+  /**
+   * Guarda el token JWT en una cookie.
+   * @param token Token JWT a guardar.
+   */
   saveToken(token: string) {
     this.cookieService.set('token', token, 1, '/', '', false, 'Strict');
   }
 
-  // Método para obtener el token de la cookie
+  /**
+   * Obtiene el token JWT de la cookie.
+   * @returns string | null - El token JWT si existe, null en caso contrario.
+   */
   getToken(): string | null {
     return this.cookieService.get('token');
   }
 
-  // Método para eliminar el token (logout)
+  /**
+   * Realiza el cierre de sesión del usuario, eliminando el token y redirigiendo a la página de inicio de sesión.
+   */
   logout() {
     this.cookieService.delete('token', '/');
     this.httpClient.post('http://localhost:5134/taller-backend/auth/logout', {}).subscribe(() => {
@@ -64,25 +107,36 @@ export class AuthService {
       // Manejar errores, si es necesario
       console.error('Error al cerrar sesión:', error);
     });
-
   }
+
+  /**
+   * Muestra una notificación indicando que la sesión se ha cerrado correctamente.
+   */
   showLogoutNotification(): void {
     alert('Sesión cerrada correctamente.');
   }
 
-  // Método para verificar si el usuario está autenticado
+  /**
+   * Verifica si el usuario está autenticado basado en el valor del BehaviorSubject isLoggedInSubject.
+   * @returns boolean - True si el usuario está autenticado, false en caso contrario.
+   */
   isAuthenticated(): boolean {
     return this.isLoggedInSubject.value;
   }
 
+  /**
+   * Verifica si el usuario tiene el rol de administrador.
+   * @returns boolean - True si el usuario es administrador, false en caso contrario.
+   */
   isAdmin(): boolean {
     const payload = this.getPayload();
     return payload && payload.rol === 'admin';
-    
   }
 
-
-  // Método para obtener la información del payload del JWT
+  /**
+   * Obtiene la información del payload del JWT almacenado en la cookie.
+   * @returns any - El payload del JWT decodificado o null si no hay token.
+   */
   getPayload(): any {
     const token = this.cookieService.get('token');
     if (token) {
@@ -92,7 +146,10 @@ export class AuthService {
     return null;
   }
 
-  // Método para agregar el token a los headers de las solicitudes protegidas
+  /**
+   * Agrega el token JWT a los encabezados de las solicitudes protegidas.
+   * @returns HttpHeaders - Los encabezados con el token JWT añadido si existe.
+   */
   addTokenToHeaders(): HttpHeaders {
     const token = this.getToken();
     if (token) {
--- src/app/paginas/login/login.component.ts
+++ src/app/paginas/login/login.component.ts
@@ -5,6 +5,12 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../jwt/auth.service';
 import { Router } from '@angular/router';
 import { RouterModule } from '@angular/router';
+
+/**
+ * Componente de inicio de sesión que maneja la autenticación del usuario.
+ * Proporciona un formulario reactivo para ingresar el correo electrónico y la contraseña,
+ * y gestiona la lógica de autenticación a través del AuthService.
+ */
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -13,30 +19,47 @@ import { RouterModule } from '@angular/router';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  /**
+   * Formulario reactivo para el inicio de sesión que incluye validaciones para el email y la contraseña.
+   */
   loginForm: FormGroup;
+
+  /**
+   * Variable para almacenar el mensaje de error en caso de fallo en el login.
+   */
   loginError: string | null = null;
 
-  constructor(private fb: FormBuilder, private authService: AuthService,  private router: Router) {
+  /**
+   * Constructor del componente LoginComponent.
+   * @param fb Instancia de FormBuilder para crear el formulario reactivo.
+   * @param authService Instancia de AuthService para manejar la autenticación.
+   * @param router Instancia de Router para la navegación dentro de la aplicación.
+   */
+  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
     });
   }
 
+  /**
+   * Método que se ejecuta al enviar el formulario de inicio de sesión.
+   */
   onSubmit() {
     if (this.loginForm.valid) {
       const { email, password } = this.loginForm.value;
 
       this.authService.login(email, password).subscribe({
         next: (response) => {
-
-  
+          // Maneja la respuesta exitosa del login
           console.log('Login exitoso:', response);
-          alert(`Bienvenido, ${response.nombre}`); 
+          alert(`Bienvenido, ${response.nombre}`); // Muestra un mensaje de bienvenida con el nombre del usuario
 
-          this.router.navigate (['/principal']);
+          // Navega a la ruta principal después de un login exitoso
+          this.router.navigate(['/principal']);
         },
         error: (err) => {
+          // Maneja los errores de login
           console.error('Error en el login:', err);
           this.loginError = err.error || 'Error desconocido al iniciar sesión.';
         },
